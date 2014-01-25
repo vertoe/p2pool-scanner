c86f984adc86ad3944652211251d96853f17fd48
donSchoe <afri.alex@gmail.com>
2014-01-25 17:46:11 +0100

    adapted for Cachecoin *fixed

--- scanner.js
+++ scanner.js
@@ -66,8 +66,9 @@ function Scanner(options) {
             +"</style>"
             +"</head><body>";
         if(logo)
-            str += "<div style='float:left;margin:16px;'><img src=\""+logo+"\" /></div><br style='clear:both;'/>";
-        str += "<center><a href='https://github.com/forrestv/p2pool' target='_blank'>PEER TO PEER "+(config.currency.toUpperCase())+" MINING NETWORK</a> - PUBLIC NODE LIST<br/><span style='font-size:10px;color:#333;'>GENERATED ON: "+(new Date())+"</span></center><p/>"
+            str += "<div style='float:left;margin:16px;'><img src=\""+logo+"\" height='30' /></div><br style='clear:both;'/>";
+        str += "<center><a href='https://github.com/Sykh/p2pool-cache' target='_blank'>PEER TO PEER "+(config.currency.toUpperCase())+" MINING NETWORK</a> - PUBLIC NODE LIST<br/><span style='font-size:10px;color:#333;'>GENERATED ON: "+(new 
+Date())+"</span></center><p/>"
         if(self.poolstats)
             str += "<center>Pool speed: "+(self.poolstats.pool_hash_rate/1000000).toFixed(2)+" "+config.speed_abbrev+"</center>";
         str += "<center>Currently observing "+(self.nodes_total || "N/A")+" nodes.<br/>"+_.size(self.addr_working)+" nodes are public with following IPs:</center><p/>";
